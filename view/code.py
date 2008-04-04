d6e03b5e8aa2df28a5ffdac5be5c9e3c944ebab0
Aaron Swartz <aaronsw@frabjous.local>
2008-04-04 13:21:31 -0700

    add web.py to REQUISITES

--- view/code.py
+++ view/code.py
@@ -1,3 +1,26 @@
 import web
 
-# placeholder for view code
+web.config.debug = True
+render = web.template.render('templates/', base='base')
+db = web.database(dbn='postgres', db='watchdog_dev', user='postgres', pw='')
+
+urls = (
+  '/', 'index',
+  '/us/([A-Z][A-Z]-\d+)', 'district',
+)
+
+class index:
+    def GET(self):
+        return render.index()
+
+class district:
+    def GET(self, district):
+        try:
+            d = db.select('district', where='name = $district', vars=locals())[0]
+        except IndexError:
+            return web.notfound()
+        
+        return render.district(d)
+
+app = web.application(urls, globals())
+if __name__ == "__main__": app.run()
