042538c372fb3e415035be91f4d0730dcbc62dc1
Aaron Swartz <me@aaronsw.com>
2008-04-14 15:31:24 -0700

    improve blog

--- blog.py
+++ blog.py
@@ -13,9 +13,15 @@ content = [
     'slug': 'launch',
     'title': 'Welcome to watchdog.net!',
     'author': 'Aaron Swartz',
-    'date': '2008-04-11',
+    'updated': '2008-04-14T00:00:00Z',
     'body': """
-<p>So we're starting a new website. You can read all about us and our plans on <a href="/about/">the about page</a> but here I mostly wanted to welcome you and apologize for the mess. We're trying to develop this site fast and in public, so expect lots of changes. We'll try to keep the public brokenness to a minimum, but there will undoubtedly be some, especially these first few weeks.</p>
+<p>It's a big election year in the US, which means a lot of people have been thinking about politics lately. I've been far from immune, signing up for dozens of sites and reading bunches of blogs. But, despite all this, I feel like there's something missing: a way for the average person to actually <em>get involved</em> in politics.</p>
+
+<p>Sure, you can be outraged over some factoid you read on a blog or take part in some action campaign started by a nonprofit, but that still feels like being a spectator to me. Instead, I wanted to a site where you could discover the facts for yourself and start your own action campaigns.</p>
+
+<p>Not finding one, I've decided to build it myself. <a href="/about#people">An amazing group of people</a> have signed on to help (<a href="/about#help">although we're still looking for more</a>) and the Sunlight Network given us a grant to fund it.</p>
+
+<p>You can read more about us and our plans on <a href="/about/">the about page</a> but for now let me just say welcome and pardon the mess. We're trying to develop this site fast and in public, so expect lots of changes. We'll try to keep the public brokenness to a minimum, but there will undoubtedly be some, especially these first few weeks.</p>
 
 <p>Thanks for bearing with us and <a href="/about/#feedback">let us know what you think</a>.</p>
 """,
@@ -30,7 +36,8 @@ class index:
 class feed:
     def GET(self):
         web.header('content-type', 'application/atom+xml')
-        return render._do('blog_feed')(content)
+        lastupdate = max(x.updated for x in content)
+        return render._do('blog_feed')(content, lastupdate)
 
 class post:
     def GET(self, name):
