38a85761cfc71ac051035f68660de0f05ddd0a81
btbytes <pradeep@btbytes.com>
2008-05-02 13:09:10 -0400

    renamed wrirerep function

--- utils/writerep.py
+++ utils/writerep.py
@@ -12,6 +12,7 @@ from urllib2 import urlopen
 from ClientForm import ParseFile
 from BeautifulSoup import BeautifulSoup
 import tempfile
+import captchasolver
 
 def has_message(soup,msg):
     bs = soup.findAll('b')
@@ -25,12 +26,16 @@ def has_message(soup,msg):
 def has_captcha(soup):
     paras = soup.findAll('p')
     for p in paras:
-        if p and p.string.find('spam') > -1:
+        if p and p.string and p.string.find('spam') > -1:
             return True
     return False
 
-
-def get_myform(url,data=None): 
+def get_challenge(soup):
+    labels =  filter(lambda x: x.get('for') == 'HIP_response', soup.findAll('label')) 
+    if labels: return labels[0].string
+    else: return None
+    
+def proc_forms(url,data=None):
     response = urlopen(url, data)
     tf = tempfile.mktemp()
     f = open(tf, 'w')
@@ -39,6 +44,10 @@ def get_myform(url,data=None):
     f.close(); f = open(tf,'r')
     forms = ParseFile(f, url)
     f.close()
+    return forms,tf
+    
+def get_myform(url,data=None): 
+    forms, tf = proc_forms(url,data)
     soup = BeautifulSoup(open(tf, 'r').read())
     try:
         os.remove(tf)
@@ -51,25 +60,31 @@ def get_myform(url,data=None):
             raise Exception("The Zip Code is not correct for the selected State.")
         elif has_message(soup, " was not found in our database."):
             raise Exception("The Zip Code was not found in our database.")
-        elif has_captcha(soup):
-            raise Exception("Captcha found")
-
+    elif has_captcha(soup):
+        challenge = get_challenge(soup)
+        form = forms[1]
+        solution = captchasolver.solve(challenge)
+        form['HIP_response'] = str(solution)
+        request = form.click()
+        form = get_myform(request.get_full_url(), request.get_data())
+        return form
     else: return forms[1]
 
-def writerep(zipcode, state, name, addr1, city, phone, email, msg, addr2='', addr3=''):
+def writerep(zipcode, state, name, addr1, city, phone, email, msg, addr2='', addr3='', zip4=''):
     states = {} 
-    form = get_myform('https://forms.house.gov/wyr/welcome.shtml')
-
+    forms,tf = proc_forms('https://forms.house.gov/wyr/welcome.shtml')
+    form = forms[1]
     # state names are in form: ``PRPuerto Rico"
     for item in form.find_control(name='state').items:
         states.update({item.name[:2]: item.name})
     
     form['state'] = [states[state]]
     form['zip'] = zipcode
+    form['zip4'] = zip4
     
     request = form.click()
     form2 = get_myform(request.get_full_url(), request.get_data())
-    
+        
     form2['name'] = name
     form2['addr1'] = addr1
     form2['addr2'] = addr2
@@ -80,15 +95,17 @@ def writerep(zipcode, state, name, addr1, city, phone, email, msg, addr2='', add
 
     request = form2.click()
     form3 = get_myform(request.get_full_url(), request.get_data())
-    
     form3['msg'] = msg
-    ## print form3
+    print form3
     ## XXX: uncomment only in production.
     ## request = form.click()
     ## response = urlopen(request.get_full_url(), request.get_data())
     ## XXX: Possibly check for "thank you" message for asserting successful dispatch of mail.
 
 if __name__ == '__main__':
-    # state='MA', zipcode='01773'
-    writerep(state='PR', zipcode='00667', name='watchdog test', addr1='111 av', city='test city',
+    # state='MA', zipcode='01773' #shared
+    # state='PR', zipcode='00667', #success
+    # state='MA', zipcode='01073', #captcha
+    
+    writerep(state='MA', zipcode='01532', name='watchdog test', addr1='111 av', city='test city',
             phone='001-001-001', email='test@watchdog.net', msg='testing...')
