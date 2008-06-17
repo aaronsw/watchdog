2581a7b9e0769a12585d4531f76a1c8529f9623f
Aaron Swartz <me@aaronsw.com>
2008-06-17 01:34:29 -0700

    add voteview files -- oops

---
+++ import/load/voteview.py
@@ -0,0 +1,24 @@
+"""
+import voteview
+"""
+
+import simplejson
+from parse import voteview
+import tools
+
+out = {}
+for pol in voteview.parse():
+    if pol.congress != 110 or not pol.get('district_id'): continue #@@
+    
+    if not tools.districtp(pol.district_id) and pol.district_id.endswith('01'):
+        pol.district_id = pol.district_id.split('-')[0] + '-' + '00'
+
+    watchdog_id = tools.districtp(pol.district_id)
+    if watchdog_id:
+        out[watchdog_id] = {
+          'icpsrid': pol.icpsr_id,
+          'nominate': pol.dim1
+        }
+
+if __name__ == "__main__":
+    print simplejson.dumps(out, indent=2, sort_keys=True)
