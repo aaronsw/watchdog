b6037ccd00a3ebfe12246ed1bd5d64c7855e7a78
Aaron Swartz <me@aaronsw.com>
2008-07-23 14:53:01 -0700

    add FEC data

---
+++ import/load/fec.py
@@ -0,0 +1,41 @@
+"""
+FEC data loader.
+"""
+from __future__ import with_statement
+import itertools
+import tools
+from tools import db
+from parse import fec
+
+fec2pol = {}
+def load_fec_ids():
+    with db.transaction():
+        db.delete('politician_fec_ids', '1=1')
+        fh = iter(file('../data/crawl/opensecrets/FEC_CRP_ID.tsv'))
+        header = fh.next()
+        for line in fh:
+            fec_id, crp_id = line.split()
+            if tools.opensecretsp(crp_id):
+                fec2pol[fec_id] = tools.opensecretsp(crp_id)
+                db.insert('politician_fec_ids',
+                  seqname=False,
+                  politician_id=tools.opensecretsp(crp_id),
+                  fec_id=fec_id)
+
+def load_fec_cans():
+    for can in fec.parse_candidates():
+        if can.candidate_id in fec2pol:
+            pol_id = fec2pol[can.candidate_id]
+            
+            total = float(can.total_receipts)
+            db.update('politician', where='id = $pol_id', vars=locals(), 
+              money_raised = can.total_receipts,
+              pct_spent = can.total_disbursements/total,
+              pct_self = can.contrib_from_candidate/total,
+              pct_indiv = can.total_indiv_contrib/total,
+              pct_pac = can.contrib_from_other_pc/total
+            )
+
+if __name__ == "__main__":
+    load_fec_ids()
+    load_fec_cans()
--- import/load/tools.py
+++ import/load/tools.py
@@ -20,7 +20,12 @@ def districtp(district):
     return _districtcache.get(district)
 
 _govtrackcache = {}
-
+_opensecretscache = {}
+def _fill():
+    for pol in db.select('politician', what='id, govtrackid, opensecretsid'):
+        _govtrackcache[pol.govtrackid] = str(pol.id)
+        _opensecretscache[pol.opensecretsid] = str(pol.id)
+        
 def govtrackp(govtrack_id):
     """
     Return the watchdog ID for a person's `govtrack_id`.
@@ -30,11 +35,15 @@ def govtrackp(govtrack_id):
         >>> print govtrackp('aosijdoisad') # ID we don't have
         None
     """
-    if not _govtrackcache:
-        for pol in db.select('politician', what='id, govtrackid'):
-            _govtrackcache[pol.govtrackid] = str(pol.id)
-
+    if not _govtrackcache: _fill()
     return _govtrackcache.get(govtrack_id)
+
+def opensecretsp(opensecrets_id):
+    """
+    Returns the watchdog ID for a person's `opensecrets_id`.
+    """
+    if not _opensecretscache: _fill()
+    return _opensecretscache.get(opensecrets_id)
 
 if __name__ == "__main__":
     import doctest
