7c8afa93080b156bd8f5eb9df3598984e67e1f8d
Devi <asldevi@gmail.com>
2008-09-05 19:44:43 +0530

    Checks for forms in frames too
    manual website entries are also passed thro' form examination and merged into wyr.json, instead of getting loaded directly into DB.

--- import/load/wyr.py
+++ import/load/wyr.py
@@ -2,13 +2,11 @@ import simplejson
 from settings import db
 
 wyr = simplejson.load(file('../data/crawl/votesmart/wyr.json'))
-wyr_manual = simplejson.load(file('../data/crawl/votesmart/wyr_manual.json'))
-wyr_manual.update(wyr)
 
 types = dict(email='E', wyr='W', ima='I', zipauth='Z')
 
 def load_wyr():
-    for distname, data in wyr_manual.iteritems():
+    for distname, data in wyr.iteritems():
             if data['contacttype'] not in types.keys(): 
                 continue
 
