0c9138c56ff87ca9aa39f771cb4f48b1d5f47859
Aaron Swartz <me@aaronsw.com>
2008-09-18 10:33:27 -0400

    garryj code review

--- import/parse/mortality.py
+++ import/parse/mortality.py
@@ -5,10 +5,8 @@ mortality data scraped from http://wonder.cdc.gov
 
 __author__ = 'garryj'
 
-import csv
+import re, csv
 import web
-import pprint
-import re
 
 def parse_line(x):
     out = web.storage()
@@ -16,34 +14,34 @@ def parse_line(x):
     out.county_fips = x['County Code']
     out.cause_name = x['Cause of death']
     out.cause_code = x['Cause of death Code']
-    if re.search(r'Suppressed', x['Count']):
+    if 'Suppressed' in x['Count']:
         out.deaths_suppressed = True
     else:
         out.deaths_suppressed = False
         out.deaths = x['Count']
     out.population = x['Population']
-    if re.search(r'Suppressed', x['Crude Rate']):
+    if 'Suppressed'  in x['Crude Rate']:
         out.crude_rate_suppressed = True
     else:
         out.crude_rate_suppressed = False
         crude_rate = re.search(r'(?P<rate>\d*(\.\d*))', x['Crude Rate'])
         if crude_rate:
             out.crude_rate = crude_rate.group('rate')
-        unreliable = re.search(r'Unreliable', x['Crude Rate'])
-        out.crude_rate_reliable = False if unreliable else True
+        unreliable = 'Unreliable' in x['Crude Rate']
+        out.crude_rate_reliable = not unreliable
     return out
 
 def parse_file(fn):
     parser = csv.DictReader(file(fn), delimiter="\t")
     for x in parser:
         # IgnoreTotal and footer lines
-        if x['Notes']=="Total" or x['Count']==None:
+        if x['Notes'] == "Total" or x['Count'] == None:
             continue
         else:
             yield parse_line(x)
 
 if __name__=="__main__":
-    import tools
     import sys
+    import tools
     for i in sys.argv[1:]:
         tools.export(parse_file(i))
