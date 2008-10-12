a23d0925ea0f4258a3c414f8f16285cb344a43e6
Devi <asldevi@gmail.com>
2008-10-12 09:59:07 +0530

    deleting before inserting

--- import/load/wyr.py
+++ import/load/wyr.py
@@ -1,28 +1,31 @@
 import simplejson as json
 from settings import db
+from __future__ import with_statement
 
 wyr = json.load(file('../data/crawl/votesmart/wyr.json'))
 
 types = dict(email='E', wyr='W', ima='I', zipauth='Z')
 
 def load_wyr():
-    for distname, data in wyr.iteritems():
-            if data['contacttype'] not in types.keys(): 
-                continue
+    with db.transaction():
+        db.delete('pol_contacts', '1=1')
+        for distname, data in wyr.iteritems():
+                if data['contacttype'] not in types.keys(): 
+                    continue
 
-            if data['contacttype'] == 'wyr':
-                contact = 'https://forms.house.gov/wyr/welcome.shtml'
-            else:
-                contact = data['contact']    
+                if data['contacttype'] == 'wyr':
+                    contact = 'https://forms.house.gov/wyr/welcome.shtml'
+                else:
+                    contact = data['contact']    
             
-            pol = db.select(politician, what='id', where='district_id=$distname', vars=locals())[0].id
-            d = {'politician':pol, 
-                    'contact':contact,
-                    'contacttype': types[data['contacttype']],
-                    'captcha': data['captcha']
-                   }
+                pol = db.select(politician, what='id', where='district_id=$distname', vars=locals())[0].id
+                d = {'politician':pol, 
+                        'contact':contact,
+                        'contacttype': types[data['contacttype']],
+                        'captcha': data['captcha']
+                       }
                    
-            db.insert('pol_contacts', seqname=False, **d)
+                db.insert('pol_contacts', seqname=False, **d)
            
 if __name__ == "__main__": 
     load_wyr()
