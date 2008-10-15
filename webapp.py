75110f0fbe5231e1247cfa29895de71ff4b619a8
Jeff Thomas <jeffdthomas@gmail.com>
2008-10-15 08:58:12 -0700

    Move call to sparkpos into template file.

--- webapp.py
+++ webapp.py
@@ -152,13 +152,7 @@ class district:
         out = apipublish.publish([d], format)
         if out: return out
         
-        pos = web.storage()
-        spark_keys = ['area_sqmi', 'est_population', 'poverty_pct', 'median_income']
-        for k in spark_keys:
-            pos[k] = sparkpos('district', k, district)
-            print >>sys.stderr, "Position of %s on %s is %d" %( district, k, pos[k])
-
-        return render.district(d, pos)
+        return render.district(d, sparkpos)
 
 def group_politician_similarity(politician_id, qmin=None):
     """Find the interest groups that vote most like a politician."""
@@ -303,17 +297,7 @@ class politician:
         out = apipublish.publish([p], format)
         if out: return out
 
-        pos = web.storage()
-        spark_keys = ['money_raised', 'pct_spent', 'pct_self', 'pct_indiv',
-                'pct_pac', 'n_earmark_requested', 'amt_earmark_requested',
-                'n_earmark_received', 'amt_earmark_received',
-                'n_bills_cosponsored', 'n_bills_introduced', 'n_bills_debated',
-                'n_bills_enacted', 'n_speeches', 'words_per_speech',
-                'nominate', 'predictability']
-        for k in spark_keys:
-            pos[k] = sparkpos('politician',k,polid)
-
-        return render.politician(p, pos)
+        return render.politician(p, sparkpos)
 
 class politician_introduced:
     def GET(self, politician_id):
