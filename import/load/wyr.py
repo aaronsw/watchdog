e56800af6b1065985afaf57fad754eae5ee213d9
Devi <asldevi@gmail.com>
2008-11-03 15:49:17 +0530

    politician contacts safe insert

--- import/load/wyr.py
+++ import/load/wyr.py
@@ -23,8 +23,10 @@ def load_wyr():
                         'contacttype': types[data['contacttype']],
                         'captcha': data['captcha']
                        }
-                   
-                db.insert('pol_contacts', seqname=False, **d)
+                try:
+                    db.insert('pol_contacts', seqname=False, **d)
+                except:
+                    continue
            
 if __name__ == "__main__": 
     load_wyr()
