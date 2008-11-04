f60fcd553e4d2e6d868893e1bcc97d6a32e37fc2
Devi <asldevi@gmail.com>
2008-11-04 18:09:02 +0530

    take off secret key from config

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
-#used to encrypt passwords
-secret_key = 'UuBCsl6nlTEjZASAbqNU'
+#used to encrypt cookies etc.
+try:
+    secret_key = file('/home/watchdog/certs/secret_key').read().strip()
+except:
+    secret_key = 'UuBCsl6nlTEjZASAbqNU' #not to get error while testing on non-server env
 #from address in mails sent by watchdog
 from_address = '"watchdog.net" <info@watchdog.net>'
 maildir_path = '/home/wathdog/Maildir'
