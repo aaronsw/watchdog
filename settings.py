5d402457afe5a0df52fd4a788575c95504b45710
Devi <asldevi@gmail.com>
2009-02-09 08:56:01 +0530

    one can load stuff from current session or all sessions depending on current_session in settings

--- settings.py
+++ settings.py
@@ -7,3 +7,5 @@ render = web.template.render('templates/', base='base', cache=production_mode)
 render_plain = web.template.render('templates/', cache=production_mode) #without base, useful for sending mails
 db = web.database(dbn=os.environ.get('DATABASE_ENGINE', 'postgres'),
                   db=os.environ.get('WATCHDOG_TABLE', 'watchdog_dev'))
+db.printing = False
+current_session = 111   #'*' to load data from all sessions
