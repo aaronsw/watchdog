1b8cbf58175a907abdf65e7153d3075352cd0a35
Devi <asldevi@gmail.com>
2009-02-18 08:39:11 +0530

    apply commify fix patch from webpy

--- vendor/webpy/web/utils.py
+++ vendor/webpy/web/utils.py
@@ -612,7 +612,7 @@ def denumify(string, pattern):
 def commify(n):
     """
     Add commas to an integer `n`.
-
+ 
         >>> commify(1)
         '1'
         >>> commify(123)
@@ -621,18 +621,34 @@ def commify(n):
         '1,234'
         >>> commify(1234567890)
         '1,234,567,890'
+        >>> commify(123.0)
+        '123.0'
+        >>> commify(1234.5)
+        '1,234.5'
+        >>> commify(1234.56789)
+        '1,234.56789'
+        >>> commify('%.2f' % 1234.5)
+        '1,234.50'
         >>> commify(None)
         >>>
-
+ 
     """
     if n is None: return None
+    n = str(n)
+    if '.' in n:
+        dollars, cents = n.split('.')
+    else:
+        dollars, cents = n, None
+ 
     r = []
-    for i, c in enumerate(reversed(str(n))):
+    for i, c in enumerate(reversed(str(dollars))):
         if i and (not (i % 3)):
             r.insert(0, ',')
         r.insert(0, c)
-    return ''.join(r)
-
+    out = ''.join(r)
+    if cents:
+        out += '.' + cents
+    return out
 def dateify(datestring):
     """
     Formats a numified `datestring` properly.
